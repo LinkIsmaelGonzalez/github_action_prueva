b87405a0a625abbe7145245f41d5f3d8d44342f8
LinkIsmaelGonzalez <ismaelagf.2019@gmail.com>
2025-07-18 23:45:05 -0600

    Confirmar y enviar cambios

--- prueva node.js
+++ prueva node.js
@@ -1,1 +1,11 @@
+const express = require('express');
+const app = express();
+const port = 3000;
 
+app.get('/',(req, res)=> {
+    res.send('¡Hola Mundo!');
+});
+
+app.listen(port,()=> {
+    console.log('Servidor Corriendo en http://Localhost:{port}');
+});
